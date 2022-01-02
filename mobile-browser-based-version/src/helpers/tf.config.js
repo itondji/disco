d430f6964d92fe41b5ed56411e186e37596fa623
Lucas Giordano <giordano3102lucas@gmail.com>
2022-01-02 15:52:42 +0100

    remove unused files

--- mobile-browser-based-version/src/helpers/tf.config.js
+++
@@ -1,6 +0,0 @@
-import * as tf_browser from '@tensorflow/tfjs';
-//import * as tf_node from '@tensorflow/tfjs-node';
-// conditional import tf for nodejs if possible (speed-up benifits)
-const tf_node = null;
-const tf = global.process.env.CLI ? tf_node : tf_browser;
-export default tf;
